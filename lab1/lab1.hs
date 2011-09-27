658456dc9d822490e6a3f2cf6a37bcaaa749c32d
Frank Chen <frankchn@gmail.com>
2011-09-27 01:26:14 -0700

    updated revision - still did not resolve string problem

--- lab1/lab1.hs
+++ lab1/lab1.hs
@@ -4,29 +4,27 @@ import Data.Char as Char
 
 split :: String -> [String]
 split [] = [""]
-split (x:xs) | (not (Char.isAlpha x)) = "" : rest
+split (x:xs) | not (Char.isAlpha x) = "" : rest
    			 | otherwise = (x : head rest) : tail rest
    			   where rest = split xs
        
-countStr :: [String] -> (Map.Map String Int) -> (Map.Map String Int)
+countStr :: [String] -> Map.Map String Int -> Map.Map String Int
 countStr [] _ = Map.empty
 countStr ("":xs) m = countStr xs m
 countStr (x:xs) m = Map.insertWith' (+) (map toLower x) 1 (countStr xs m)
 
-genHistogram :: Int -> String
-genHistogram n = genLoop [0..n] "" where 
-				 genLoop (x:xs) s = genLoop xs ("*" ++ s)
-				 genLoop [] s = s
+genStars :: Int -> String
+genStars n = replicate n '*'
 
 collapse :: String -> String -> Int -> String 
-collapse result k a = result ++ k ++ " " ++ (genHistogram a) ++ "\n"
+collapse result k a = result ++ k ++ " " ++ genStars a ++ "\n"
 
-doEverything :: String -> String
-doEverything s = Map.foldlWithKey collapse "" (countStr (split s) Map.empty)
+genHistogram :: String -> String
+genHistogram s = Map.foldlWithKey collapse "" (countStr (split s) Map.empty)
 
 main = do
   args <- getArgs
-  mapM_ (interactFile doEverything) args
+  mapM_ (interactFile genHistogram) args
  
 interactFile f fileName = do
   s <- readFile fileName
